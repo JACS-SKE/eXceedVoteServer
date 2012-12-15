ec02dd16d13126c2c4c40cbaa20404c540e51c31
nookskill <nookskill@hotmail.com>
2012-12-15 13:58:56 +0700

    test

--- src/jacs/Main.java
+++ src/jacs/Main.java
@@ -7,8 +7,8 @@ import jacs.server.Server;
 
 /**
  * This class Main for Running Server for eXceedVote.
- * @author GoDParTicle,nookskill
- *
+ * @author GoDParTicle
+ * @author nookskill
  */
 
 public class Main {
